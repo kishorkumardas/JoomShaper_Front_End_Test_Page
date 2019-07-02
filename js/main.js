2cfe165c3f5035a86ad7b5e02ed033ccb098031b
kishorkumardas <kishorworld16@gmail.com>
2019-07-03 01:40:49 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $('#clock').countdown("2019/05/08", function(event) {
+      var totalHours = event.offset.totalDays * 24 + event.offset.hours;
+       $('.hours').html(totalHours);
+       $('.minutes').html(event.strftime('%M'));
+       $('.seconds').html(event.strftime('%S'));
+     });
+     
+     $(".slider-active").owlCarousel({
+      loop: true,
+      nav: true,
+      dots:false,
+      autoplay: true,
+      autoplayTimeout:3000,
+      navText: ['<i class="fas fa-arrow-right"></i>','<i class="fas fa-arrow-left"></i>'],
+      responsive: {
+            0: {
+              items: 1
+          },
+          767: {
+              items: 1
+          },
+          768:{
+              items:2
+          },
+          992: {
+              items: 3
+          },
+          1200: {
+              items: 3
+          }
+      }
+    
+      
+  });
+  });
